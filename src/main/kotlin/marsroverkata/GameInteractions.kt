54efa09654c0dc93822925e8ce2c2b557fe1f626
eureka84 <angelosciarra@ymail.com>
2018-12-18 08:22:24 +0100

    refactored functions to val lamdas

--- src/main/kotlin/marsroverkata/GameInteractions.kt
+++ src/main/kotlin/marsroverkata/GameInteractions.kt
@@ -4,33 +4,29 @@ import arrow.effects.IO
 import marsroverkata.Data.Command
 import marsroverkata.Data.Planet
 import marsroverkata.Data.Position
-import marsroverkata.IoOps.ask
-import marsroverkata.IoOps.puts
 import marsroverkata.DataParsers.parseCommands
 import marsroverkata.DataParsers.parseObstacles
 import marsroverkata.DataParsers.parsePlanet
 import marsroverkata.DataParsers.parsePosition
+import marsroverkata.IoOps.ask
+import marsroverkata.IoOps.puts
 
 object GameInteractions {
 
-    fun welcome() = puts("Welcome to the Mars Rover Kata!")
+    val welcome: () -> IO<Unit> = { puts("Welcome to the Mars Rover Kata!") }
 
-    fun readPlanet(): IO<Planet> =
-            ask("What is the size of the planet?").map(parsePlanet)
+    val readPlanet: () -> IO<Planet> = { ask("What is the size of the planet?").map(parsePlanet) }
 
-    fun readObstacles(): IO<List<Position>> =
-            ask("Where are the obstacles?").map(parseObstacles)
+    val readObstacles: () -> IO<List<Position>> = { ask("Where are the obstacles?").map(parseObstacles) }
 
-    fun readPosition(): IO<Position> =
-            ask("What is the position of the rover?").map(parsePosition)
+    val readPosition: () -> IO<Position> = { ask("What is the position of the rover?").map(parsePosition) }
 
-    fun readCommands(): IO<List<Command>> =
-            ask("Waiting commands...").map(parseCommands)
+    val readCommands: () -> IO<List<Command>> = { ask("Waiting commands...").map(parseCommands) }
 
-    fun display(result: Result): IO<Unit> {
+    val display: (Result) -> IO<Unit> = { result ->
         val (hitObstacle, rover) = result
         val prefix = if (hitObstacle) "O:" else ""
-        return puts("$prefix${rover.direction}:${rover.position.x},${rover.position.y}")
+        puts("$prefix${rover.direction}:${rover.position.x},${rover.position.y}")
     }
 
 }
