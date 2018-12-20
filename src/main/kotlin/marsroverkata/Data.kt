88c929a3e6c2f361828d25ddb75d91c9b0747d9b
eureka84 <angelosciarra@ymail.com>
2018-12-20 08:24:03 +0100

    minor

--- src/main/kotlin/marsroverkata/Data.kt
+++ src/main/kotlin/marsroverkata/Data.kt
@@ -1,6 +1,8 @@
 package marsroverkata
 
-data class Planet(val width: Int, val height: Int, val obstacles: List<Position> = listOf())
+val Nil = listOf<Nothing>()
+
+data class Planet(val width: Int, val height: Int, val obstacles: List<Position> = Nil)
 data class Position(val x: Int, val y: Int)
 data class Rover(val position: Position, val direction: Direction, val planet: Planet)
 
